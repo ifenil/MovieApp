d25496a72bddfadd384c36acbfc660dc47ebd15d
ifenil <fenil.dobariya@outlook.com>
2022-08-25 20:55:31 +0530

    Google signin added

--- app/src/main/java/com/movie/app/MainActivity.kt
+++ app/src/main/java/com/movie/app/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.movie.app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
+import com.movie.app.LoadingState.Companion.LOADING
 import com.movie.app.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
+    private val loginViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    GoogleSignInComponent(loginViewModel = loginViewModel)
                 }
             }
         }
@@ -27,14 +40,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GoogleSignInComponent(loginViewModel: MainViewModel) {
+
+    val state by loginViewModel.loadingState.collectAsState()
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                val credential = GoogleAuthProvider.getCredential(account.idToken!!, null)
+                loginViewModel.signWithCredential(credential)
+            } catch (e: ApiException) {
+                Log.e("TAG", "Google sign in failed", e)
+            }
+        }
+
+    val context = LocalContext.current
+    val token = stringResource(R.string.default_web_client_id)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = {
+            OutlinedButton(border = ButtonDefaults.outlinedBorder.copy(width = 1.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp), onClick = {
+                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(token)
+                        .requestEmail()
+                        .build()
+
+                    val googleSignInClient = GoogleSignIn.getClient(context, gso)
+                    launcher.launch(googleSignInClient.signInIntent)
+                },
+                content = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = if (state == LOADING) Arrangement.Center else Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        content = {
+                            if (state == LOADING) {
+                                CircularProgressIndicator()
+                            } else {
+                                Text(
+                                    style = MaterialTheme.typography.button,
+                                    color = MaterialTheme.colors.onSurface,
+                                    text = "Sign in with Google"
+                                )
+                            }
+                        }
+                    )
+                })
+
+            when (state.status) {
+                LoadingState.Status.SUCCESS -> {
+
+                    Text(text = "Success")
+                }
+                LoadingState.Status.FAILED -> {
+                    Text(text = state.msg ?: "Error")
+                }
+                LoadingState.Status.LOGGED_IN -> {
+                    Text(text = "Already Logged In")
+                }
+                else -> {
+                }
+            }
+        }
+    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/movie/app/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.movie.app
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+data class LoadingState private constructor(val status: Status, val msg: String? = null) {
+    companion object {
+        val LOADED = LoadingState(Status.SUCCESS)
+        val IDLE = LoadingState(Status.IDLE)
+        val LOADING = LoadingState(Status.RUNNING)
+        val LOGGED_IN = LoadingState(Status.LOGGED_IN)
+        fun error(msg: String?) = LoadingState(Status.FAILED, msg)
+    }
+
+    enum class Status {
+        RUNNING,
+        SUCCESS,
+        FAILED,
+        IDLE,
+        LOGGED_IN
+    }
+}
+
+class MainViewModel : ViewModel() {
+    val loadingState = MutableStateFlow(LoadingState.IDLE)
+
+    fun signWithCredential(credential: AuthCredential) = viewModelScope.launch {
+        try {
+            loadingState.emit(LoadingState.LOADING)
+            Firebase.auth.signInWithCredential(credential)
+            loadingState.emit(LoadingState.LOADED)
+        } catch (e: Exception) {
+            loadingState.emit(LoadingState.error(e.localizedMessage))
+        }
+    }
+}
